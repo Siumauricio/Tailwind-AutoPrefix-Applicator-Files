07b4b0de35de12103de5410fc653b906c4e6cf76
Siumauricio <Siumauricio@unitec.edu>
2022-01-04 17:59:52 -0600

    ports

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({extended: false, limit: "50mb"}));
 
