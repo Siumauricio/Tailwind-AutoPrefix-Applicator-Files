88be3bf4b67b0fc7d66b7c14aa776d3e359f249b
Siumauricio <Siumauricio@unitec.edu>
2022-01-04 17:56:32 -0600

    added config settings for deploy

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const fs = require("fs");
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 5000;
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({extended: false, limit: "50mb"}));
 
